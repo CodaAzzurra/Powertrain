0c4b8c6b36ae65fa0c10f140803d4d8595fb3796
davidfelcey <davefelcey@gmail.com>
2016-04-07 20:46:10 +0100

    Can now send speed and accleration in REST calls and now store DAO, session and cluster objects in servlet context

--- src/main/java/com/datastax/demo/vehicle/webservice/VehicleService.java
+++ src/main/java/com/datastax/demo/vehicle/webservice/VehicleService.java
@@ -3,17 +3,16 @@ package com.datastax.demo.vehicle.webservice;
 import com.datastax.demo.vehicle.VehicleDao;
 import com.datastax.demo.vehicle.model.Location;
 import com.datastax.demo.vehicle.model.Vehicle;
-import com.github.davidmoten.geo.LatLong;
 
-import java.util.HashMap;
+import javax.servlet.ServletContext;
 import java.util.List;
-import java.util.Map;
 
 public class VehicleService {
 
-    private static final VehicleDao dao = new VehicleDao(System.getProperty("contactPoints", "127.0.0.1").split(","));
+    private VehicleDao dao = null;
 
-    public VehicleService() {
+    public VehicleService(ServletContext context) {
+        dao = new ContextSessionService(context).getDAO();
     }
 
     public List<Vehicle> getVehicleMovements(String vehicle, String dateString) {
@@ -32,10 +31,8 @@ public class VehicleService {
         return dao.getVehiclesLocation(vehicleId);
     }
 
-    public void updateVehicleLocation(String vehicleId, Location location) {
-        Map<String, Location> newLocation = new HashMap<String, Location>(1);
-        newLocation.put(vehicleId, location);
-        dao.insertVehicleLocation(newLocation);
+    public void updateVehicleLocation(String vehicleId, Location location, int speed, int acceleration) {
+        dao.updateVehicle(vehicleId, location, speed, acceleration);
     }
 
     public void addVehicleEvent(String vehicleId, String eventName, String eventValue) {
